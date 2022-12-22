9ceab2909b44dc737a9ba4ce0d27a21c4df2280b
Elvira <elviraskerova@gmail.com>
2022-12-22 22:36:15 +0300

    demoqa refresh

--- demoqa_test.py
+++ demoqa_test.py
@@ -10,40 +10,57 @@ def test_student_registration_form():
     browser.element('#firstName').type('Elvira')
     browser.element('#lastName').type('Askerova')
     browser.element('#userEmail').type('elviraskerova@gmail.com')
-    browser.element('[name=gender][value=Female]').click()
+    browser.all('[name=gender]').element_by(have.value('Female')).element('./following-sibling::*').click()
     browser.element('#userNumber').type('1234567891')
 
     browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker_month-select').type('December')
-    browser.element('.react-datepicker_year-select').type('1990')
-    browser.element(f'.react-datepicker_day--0{8}').click()
+    browser.element('.react-datepicker__month-select').type('December')
+    browser.element('.react-datepicker__year-select').type('1990')
+    browser.element(f'.react-datepicker__day--00{8}').click()
 
     browser.element('#subjectsInput').type('Commerce').press_enter()
 
     browser.element('[for=hobbies-checkbox-2]').click()
 
-    browser.element('#uploadPicture').send_keys(file_path)
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    file_path.join(current_dir, 'attachments/photo.jpeg')
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), 'attachments/photo.jpeg')))
+
 
     browser.element('#currentAddress').type('Plehanova Street 1')
-    browser.element('#state').click()
-    browser.all('[id^=react-select][id*=options]').element_by(have.text('Uttar Pradesh')).click()
-    browser.element('#city').click()
-    browser.all('[id^=react-select][id*=options]').element_by(have.text('Agra')).click()
+    # browser.element('#state').click()
+    # browser.all('[id^=react-select][id*=options]').element_by(have.text('Uttar Pradesh')).click()
+    # browser.element('#city').click()
+    # browser.all('[id^=react-select][id*=options]').element_by(have.text('Agra')).click()
+    browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
+    browser.element('#react-select-4-input').type('Agra').press_enter()
+    browser.element('#submit').press_enter()
 
     #THEN
-    browser.element('.table').all('td:nth-of-type(2').should()
-    have.text(
-        'Elvira Askerova',
-        'elviraskerova@gmail.com',
-        'Female',
-        '123456789',
-        '8 December, 1990',
-        'Commerce',
-        'Reading',
-        'foto.jpg',
-        'Plehanova Street 1',
-        'Uttar Pradesh',
-        'Agra',
-    )
+    # browser.element('.table').all('td:nth-of-type(2').should(
+    #     have.text(
+    #         'Elvira Askerova'
+    #         and 'elviraskerova@gmail.com'
+    #         and 'Female'
+    #         and '123456789'
+    #         and '8 December, 1990'
+    #         and 'Commerce'
+    #         and 'Reading'
+    #         and 'foto.jpg'
+    #         and 'Plehanova Street 1'
+    #         and 'Uttar Pradesh'
+    #         and 'Agra'
+    #     ))
+
+    browser.element('.table').should(have.text(
+            'Elvira Askerova'
+            and 'elviraskerova@gmail.com'
+            and 'Female'
+            and '123456789'
+            and '8 December, 1990'
+            and 'Commerce'
+            and 'Reading'
+            and 'foto.jpg'
+            and 'Plehanova Street 1'
+            and 'Uttar Pradesh'
+            and 'Agra'
+        ))
