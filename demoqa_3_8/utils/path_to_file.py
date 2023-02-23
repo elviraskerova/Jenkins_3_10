9370b1e71802a31f9c0c87a43b574f03f2cfcc40
Elvira <elviraskerova@gmail.com>
2023-02-23 23:20:16 +0300

    demoqa refresh4

---
+++ demoqa_3_8/utils/path_to_file.py
@@ -0,0 +1,12 @@
+from selene.support.shared import browser
+import os
+import attachments
+
+
+def create_path(selector, path):
+    browser.element(selector).set_value(
+        os.path.abspath(
+            os.path.join(os.path.dirname(attachments.__file__), path)
+        )
+    )
+
