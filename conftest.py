358a0cabab43d86d79d85fb9a8ed99071ebfc431
Elvira <elviraskerova@gmail.com>
2023-03-11 16:35:19 +0300

    refresh

--- conftest.py
+++ conftest.py
@@ -28,6 +28,8 @@ def setup_browser():
     browser.config.window_width = 2000
     browser.config.window_height = 1500
 
+    browser.config.base_url = 'https://demoqa.com'
+
     yield browser
 
     attach.add_html(browser)
