fbd3e889cf3255a1018bdfbeb096dee8bfb35596
Elvira <elviraskerova@gmail.com>
2023-04-20 00:00:53 +0300

    изменение пути файла

--- demoqa_3_8/utils/work_with_path.py
+++ demoqa_3_8/utils/work_with_path.py
@@ -1,9 +1,9 @@
 import os
-import tests
+import attachments
 
 
 def get_path(relative_path):
     abs_path = os.path.abspath(
-        os.path.join(os.path.dirname(tests.__file__), relative_path))
+        os.path.join(os.path.dirname(attachments.__file__), relative_path))
 
     return abs_path
